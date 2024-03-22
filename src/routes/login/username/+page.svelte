60762c14309194b2cb25f7a24c97251343185159
Ryan Gibson <ryanpc@ryanpc.net>
2024-03-22 14:22:51 -0600

    added form validation

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-    import { getFirebaseContext, SignedIn, userStore } from "sveltefire";
+    import { SignedIn, userStore } from "sveltefire";
     import { doc, getDoc, writeBatch } from "firebase/firestore";
-    // const { auth, firestore } = getFirebaseContext();
     import { auth, firestore } from "$lib/firebase";
 
     const user = userStore(auth);
@@ -29,42 +28,71 @@
         }, 500);
     }
 
-        async function confirmUsername() {
-            const batch = writeBatch(firestore);
-            batch.set(doc(firestore, "usernames", username), { uid: $user?.uid });
-            batch.set(doc(firestore, "users", $user!.uid), {
-                username,
-                photoURL: $user?.photoURL ?? null,
-                published: true,
-                bio: "I am the Walrus",
-                links: [
-                    {
-                        title: "Test Link",
-                        url: "https://kung.foo",
-                        icon: "custom",
-                    },
-                ],
-            });
-            await batch.commit();
+    async function confirmUsername() {
+        const batch = writeBatch(firestore);
+        batch.set(doc(firestore, "usernames", username), { uid: $user?.uid });
+        batch.set(doc(firestore, "users", $user!.uid), {
+            username,
+            photoURL: $user?.photoURL ?? null,
+            published: true,
+            bio: "I am the Walrus",
+            links: [
+                {
+                    title: "Test Link",
+                    url: "https://kung.foo",
+                    icon: "custom",
+                },
+            ],
+        });
+        await batch.commit();
 
-            username = "";
-            isAvailable = false;
+        username = "";
+        isAvailable = false;
     }
+
+    const re = /^(?=[a-zA-Z0-9._]{3,16}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+
+    $: isValid =
+        username?.length > 2 && username.length < 16 && re.test(username);
+    $: isTouched = username.length > 0;
+    $: isTaken = isValid && !isAvailable && !loading;
+    
 </script>
 
 <SignedIn let:user>
     <h2>Username</h2>
     <form class="w-2/5" on:submit|preventDefault={confirmUsername}>
         <input
-            type="text"
-            placeholder="Username"
-            class="input w-full"
-            bind:value={username}
-            on:input={checkAvailability}
-        />
+        type="text"
+        placeholder="Username"
+        class="input w-full"
+        bind:value={username}
+        on:input={checkAvailability}
+        class:input-error={(!isValid && isTouched)}
+        class:input-warning={isTaken}
+        class:input-success={isAvailable && isValid && !loading}
+      />
 
-        <p>Is available? {isAvailable}</p>
-
-        <button class="btn btn-success">Confirm username @{username} </button>
+      <div class="my-4 min-h-16 px-8 w-full">
+        {#if loading}
+          <p class="text-secondary">Checking availability of @{username}...</p>
+        {/if}
+    
+        {#if !isValid && isTouched}
+          <p class="text-error text-sm">
+            must be 3-16 characters long, alphanumeric only
+          </p>
+        {/if}
+    
+        {#if isValid && !isAvailable && !loading}
+          <p class="text-warning text-sm">
+            @{username} is not available
+          </p>
+        {/if}
+    
+        {#if isAvailable}
+          <button class="btn btn-success">Confirm username @{username} </button>
+        {/if}
+      </div>
     </form>
 </SignedIn>
