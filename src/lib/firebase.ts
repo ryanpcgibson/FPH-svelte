075cb31597e7c535292fd9aeeb8a14cb38a8e901
Ryan Gibson <ryanpc@ryanpc.net>
2024-03-29 19:11:50 -0600

    Seems to be working with docStore and userStore by sveltefire

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,74 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth, onAuthStateChanged, type Auth, type User } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-import { writable, type Readable, derived } from "svelte/store";
-// import { userStore } from "sveltefire";
-import { doc, onSnapshot } from "firebase/firestore";
+import { type Readable, derived } from "svelte/store";
+import { userStore, docStore } from "sveltefire";
 import firebaseConfig from './firebaseConfig.json';
 
 export const app = initializeApp(firebaseConfig);
 export const firestore = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
-
-/**
- * @returns a store with the current firebase user
- */
-function userStore(auth: Auth) {
-  let unsubscribe: () => void;
-
-  if (!auth || !globalThis.window) {
-    console.warn('Auth is not initialized or not in browser');
-    const { subscribe } = writable<User | null>(null);
-    return {
-      subscribe,
-    }
-  }
-
-  const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
-    unsubscribe = onAuthStateChanged(auth, (user) => {
-      set(user);
-    });
-
-    return () => unsubscribe();
-  });
-
-  return {
-    subscribe,
-  };
-}
-
-
 export const user = userStore(auth);
 
-
-/**
- * @param  {string} path document path or reference
- * @returns a store with realtime updates on document data
- */
-export function docStore<T>(
-  path: string,
-) {
-  let unsubscribe: () => void;
-
-  const docRef = doc(firestore, path);
-
-  const { subscribe } = writable<T | null>(null, (set) => {
-    unsubscribe = onSnapshot(docRef, (snapshot) => {
-      set((snapshot.data() as T) ?? null);
-    });
-
-    return () => unsubscribe();
-  });
-
-  return {
-    subscribe,
-    ref: docRef,
-    id: docRef.id,
-  };
-}
-
+// Extra user data beyond what auth provides
 interface UserData {
   username: string;
   bio: string;
@@ -80,7 +24,7 @@ interface UserData {
 export const userData: Readable<UserData | null> = derived(user, ($user, set) => {
   if ($user) {
     console.log('firebase.ts userData $user', $user);
-    return docStore<UserData>(`users/${$user.uid}`).subscribe(set);
+    return docStore<UserData>(firestore, `users/${$user.uid}`).subscribe(set);
   } else {
     set(null);
   }
