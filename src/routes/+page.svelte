da7abebee3f72602de53b190338e5b5398955b51
Ryan Gibson <ryanpc@ryanpc.net>
2024-03-31 20:41:25 -0600

    Debugging dev/prod delta

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    const buildTime = process.env.BUILD_TIME;
+</script>
+
+<h1>Build Time</h1>
+<p>This app was built at: {buildTime}</p>
+<p>Environment: {process.env.NODE_ENV}</p>
--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	define: {
+		'process.env.BUILD_TIME': JSON.stringify(new Date().toISOString()),
+		'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+
+	  }
 });
